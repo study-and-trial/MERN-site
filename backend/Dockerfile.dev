b54260d6e848c886ed5d8a0b3f9ea8235e09bcfa
sangyeon kim <greyhairchooselife@gmail.com>
2024-05-23 19:49:18 +0900

    cicd: 개발환경 Dockerfile, docker-compose file, 스크립트
    - 스크립트는 docker-compose를 편리하게 조작하는 interactive program
    - `--watch` 는 docker-compose의 command 또는 up command의 flag로 사용할
      수 있다. 이름 그대로 변경내역을 살펴보는것.
      - config file에 develop:watch: 키워드를 반드시 넣어줘야한다.
    
      ex)
      ```docker-compose.yml
        develop:
          watch:
            - action: sync
              path: ./backend/
              target: /app/
              ignore:
                - ./backend/node_modules
      ```

---
+++ backend/Dockerfile.dev
@@ -0,0 +1,9 @@
+# https://hub.docker.com/_/node
+FROM node:22-alpine
+WORKDIR /app
+
+RUN npm install -g pnpm
+COPY package.json pnpm-lock.yaml ./
+RUN pnpm install
+
+COPY . .
---
+++ run-dev.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+PROJECT_NAME="dev-fbd-site"
+CONFIG_FILE="docker-compose.dev.yml"
+UP_OPTIONS="--build --watch"
+
+
+choose_action() {
+  echo -e ""
+  echo -e "  1. Up (build and watch)"
+  echo -e "  2. Down"
+  echo -e "  3. Restart (donw and up)"
+  echo -e "  4. Status of Projects, Containers\n"
+  read -p "Enter your choice: " choice
+  echo -e ""
+
+  case $choice in
+    1)
+      docker compose --project-name $PROJECT_NAME --file $CONFIG_FILE up $UP_OPTIONS
+      choose_action
+      ;;
+    2)
+      docker compose --project-name $PROJECT_NAME down
+      choose_action
+      ;;
+    3)
+      docker compose --project-name $PROJECT_NAME down
+      docker compose --project-name $PROJECT_NAME --file $CONFIG_FILE up $UP_OPTIONS
+      choose_action
+      ;;
+    4)
+      docker compose ls --all --filter name=$PROJECT_NAME
+      echo -e ""
+      docker compose -p $PROJECT_NAME ps --all
+      choose_action
+      ;;
+    *)
+      echo "Invalid choice"
+      choose_action
+      ;;
+  esac
+}
+
+choose_action
