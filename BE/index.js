c3fdece8d59341b4b33e0e63ff61ce70fe8c23e6
sangyeon kim <greyhairchooselife@gmail.com>
2024-05-26 23:20:36 +0900

    [FE]feat: feat: api-server에서 items 가져오기

--- BE/index.js
+++ BE/index.js
@@ -1,11 +1,13 @@
 import express from "express";
+import cors from "cors";
 const app = express();
 const PORT = 5001;
 import db from "./configs/database.js";
 
 app.use(express.json()); // 모든 express 프로젝트가 json을 사용하는것은 아니다.
+app.use(cors());
 
-app.get("/item", async (_req, res) => {
+app.get("/items", async (_req, res) => {
   const [rows] = await db.query("SELECT * FROM devices");
 
   res.status(200).json(rows);
--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,7 +1,44 @@
+import { useState } from "react";
+import axios from "axios";
+
+const API_SERVER_SERVICE_PORT = import.meta.env
+  .VITE_APP_API_SERVER_SERVICE_PORT;
+const API_SERVER_HOST = `http://localhost:${API_SERVER_SERVICE_PORT}`;
+
+interface Item {
+  id: number;
+  name: string;
+  price: number;
+  created_at: Date;
+}
+
 function App() {
+  const [items, setItems] = useState<Item[]>();
+
+  const getItems = async () => {
+    try {
+      const { data } = await axios.get(`${API_SERVER_HOST}/items`);
+
+      const items = data.map((item: any) => ({
+        ...item,
+        created_at: new Date(item.created_at),
+      }));
+
+      setItems(items);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
-      <p>hello world!</p>
+      <button onClick={getItems}>get itmes</button>
+      <h2>List of items:</h2>
+      {items?.map((item) => (
+        <p key={item.id}>
+          {item.created_at.toLocaleDateString()}: {item.name}, {item.price}
+        </p>
+      ))}
     </>
   );
 }
